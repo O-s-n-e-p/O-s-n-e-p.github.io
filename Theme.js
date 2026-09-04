24c8c9f3fdeb3c4ef80b766f6dbcc85fcd8e3749
O-s-n-e-p <oliveralijustyna@gmail.com>
2026-09-04 11:58:16 -0400

    i love florida

--- Theme.js
+++ Theme.js
@@ -3,10 +3,10 @@ const savedTheme = localStorage.getItem("theme");
 
 if (savedTheme) {
     console.log("yes in theme.js");
-    document.documentElement.classList.add(savedTheme.toLowerCase() + "-theme");
+    document.body.classList.add(savedTheme.toLowerCase() + "-theme");
 }
 else {
     console.log("no in theme.js");
     localStorage.setItem("theme", "classic");
-    document.documentElement.classList.add("classic-theme");
+    document.body.classList.add("classic-theme");
 }
